c92d8a3e416d42eb7eb2fe61682c3f72e9a5af28
nullthefirst <jrandangana@gmail.com>
2018-12-05 09:49:21 +0100

    added web art piece

---
+++ art/honey-and-ice/action.js
@@ -0,0 +1,38 @@
+function one() {
+  document.getElementById("one").style.backgroundColor = "#fff";
+}
+function two() {
+  document.getElementById("two").style.backgroundColor = "#000";
+}
+function three() {
+  document.getElementById("three").style.backgroundColor = "#fff";
+}
+function four() {
+  document.getElementById("four").style.backgroundColor = "#000";
+}
+function five() {
+  document.getElementById("five").style.backgroundColor = "#fff";
+}
+function six() {
+  document.getElementById("six").style.backgroundColor = "#000";
+}
+function seven() {
+  document.getElementById("seven").style.backgroundColor = "#fff";
+}
+function eight() {
+  document.getElementById("eight").style.backgroundColor = "#000";
+}
+function nine() {
+  document.getElementById("nine").style.backgroundColor = "#fff";
+}
+function art() {
+  document.getElementById("one").style.backgroundColor = "#eb9605";
+  document.getElementById("two").style.backgroundColor = "#f0f0f0";
+  document.getElementById("three").style.backgroundColor = "#eb9605";
+  document.getElementById("four").style.backgroundColor = "#f0f0f0";
+  document.getElementById("five").style.backgroundColor = "#eb9605";
+  document.getElementById("six").style.backgroundColor = "#f0f0f0";
+  document.getElementById("seven").style.backgroundColor = "#eb9605";
+  document.getElementById("eight").style.backgroundColor = "#f0f0f0";
+  document.getElementById("nine").style.backgroundColor = "#eb9605";
+}
